dcbce1323ec5734cfaa80a9dc8635bed01dc1399
Demetrio da Silva Passos <zinhoflag1@gmail.com>
2021-05-20 16:16:16 -0300

    20.05.2021

--- base/copy.php
+++ base/copy.php
@@ -5,23 +5,31 @@
 $tabela = isset($_POST['tabela']) ? $_POST['tabela'] : "default";
 $tabelaUc = ucfirst($tabela);
 
+$caminho = $_POST['txtBase'] ? $_POST['txtBase'] : "";
+$modulo = $_POST['modulo'] ? $_POST['modulo'] : "";
+$bacFront = $_POST['bacfront'] ? $_POST['bacfront'] : ""; 
+$view = $_POST['view'] ? $_POST['view'] : "";
+$controller = $_POST['controller'] ? $_POST['controller'] : "";
+$model = $_POST['model'] ? $_POST['model'] : "";
+$contexto = $_POST['contexto'] ? $_POST['contexto'] : "";
+
 $path = $_SERVER['DOCUMENT_ROOT'];
 
-if (!file_exists("C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/{$tabela}/")) {
-                mkdir("C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/{$tabela}/");
+if (!file_exists($caminho."/".$modulo."/".$bacFront."/".$view."/".$contexto."/".$tabela."/")) {
+                mkdir($caminho."/".$modulo."/".bacFront."/".$view."/".$contexto."/".$tabela."/");
             }
 
 try{
 
-copy ($path."/geradorsdc/arquivo/model/".$tabelaUc."ConEstoqueModel.php", "C:/Users\zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/Model/{$tabelaUc}ConEstoqueModel.php");
-
-copy ($path."/geradorsdc/arquivo/controller/".$tabela."Controller.php", "C:\Users\zinhoflag1\Documents\DOWNLOAD\wamp64\www\gestaocedec\mod_ajuda\backEnd\Controller/{$tabela}Controller.php");
-copy ($path."/geradorsdc/arquivo/crud/".$tabela."/cadastro.php", "C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/$tabela/cadastro.php");
-copy ($path."/geradorsdc/arquivo/crud/".$tabela."/edit.php",     "C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/$tabela/edit.php");
-copy ($path."/geradorsdc/arquivo/crud/".$tabela."/index.php",    "C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/$tabela/index.php");
-copy ($path."/geradorsdc/arquivo/crud/".$tabela."/view.php",     "C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/$tabela/view.php");
-copy ($path."/geradorsdc/arquivo/crud/".$tabela."/pesquisa.php",     "C:/Users/zinhoflag1/Documents/DOWNLOAD/wamp64/www/gestaocedec/mod_ajuda/backEnd/View/conEstoque/$tabela/pesquisa.php");
-
+copy ($path."/geradorsdc/arquivo/model/".$tabelaUc.$contexto.".php", $caminho."\gestaocedec/mod_ajuda/Model/{$tabelaUc}ConEstoqueModel.php");
+/*
+copy ($path."/geradorsdc/arquivo/controller/".$tabela."Controller.php", $caminho."\mod_ajuda\backEnd\Controller/{$tabela}Controller.php");
+copy ($path."/geradorsdc/arquivo/crud/".$tabela."/cadastro.php", $caminho."/mod_ajuda/backEnd/View/conEstoque/$tabela/cadastro.php");
+copy ($path."/geradorsdc/arquivo/crud/".$tabela."/edit.php",     $caminho."/mod_ajuda/backEnd/View/conEstoque/$tabela/edit.php");
+copy ($path."/geradorsdc/arquivo/crud/".$tabela."/index.php",    $caminho."/mod_ajuda/backEnd/View/conEstoque/$tabela/index.php");
+copy ($path."/geradorsdc/arquivo/crud/".$tabela."/view.php",     $caminho."/mod_ajuda/backEnd/View/conEstoque/$tabela/view.php");
+copy ($path."/geradorsdc/arquivo/crud/".$tabela."/pesquisa.php",     $caminho."/mod_ajuda/backEnd/View/conEstoque/$tabela/pesquisa.php");
+/*
 /* injetar model include */
 
 
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php include_once ("classe/Classe.PDO.php"); 
-    include_once("database.php");
-    
-    $dados = Database::getDatabase();
-    
+<?php
+include_once ("classe/Classe.PDO.php");
+include_once("database.php");
+
+$dados = Database::getDatabase();
+$tabelas = Database::getTabelas('gestaocedec');
 ?>
 <!doctype html>
 <html lang="en">
@@ -25,59 +26,57 @@
         </div>
 
 
-        <div class="container">
-
-            <div class="col-md-12">
-                <p class="text-center"><legend>Gerador de Código</legend></p>
-            </div>
+        <div class="col-md-12">
+            <p class="text-center"><legend>Gerador de Código</legend></p>
+    </div>
 
 
-            <!--<form action="#" method="post" accept-charset="utf-8">
-                    <label>
-                            host:
-                            <input type="text" name="host" id="host" value="http://localhost" >
-                    </label>
-                    <label>
-                            porta:
-                            <input type="text" name="porta" id="porta" value="3306" >
-                    </label>
-                    <label>
-                            usuario:
-                            <input type="text" name="usuario" id="usuario" value="usuario" >
-                    </label>
-                    <label>
-                            senha:
-                            <input type="text" name="senha" id="senha" value="usuario" >
-                    </label>
-    
-                    <button type="submit" name="envia">Conexão</button>
-            </form>-->
-            
-            <label>CaminhoBase</label>
-            <input type="text" id="txtBase" name=''
-                   
-                
+    <!--<form action="#" method="post" accept-charset="utf-8">
+            <label>
+                    host:
+                    <input type="text" name="host" id="host" value="http://localhost" >
+            </label>
+            <label>
+                    porta:
+                    <input type="text" name="porta" id="porta" value="3306" >
+            </label>
+            <label>
+                    usuario:
+                    <input type="text" name="usuario" id="usuario" value="usuario" >
+            </label>
+            <label>
+                    senha:
+                    <input type="text" name="senha" id="senha" value="usuario" >
+            </label>
 
-            <form action="#" method="post" name="frmGerar" id="frmGerar">
-                <div class="col-md-6">
+            <button type="submit" name="envia">Conexão</button>
+    </form>-->
+    <div class="container">
+        <form action="#" method="post" name="frmGerar" id="frmGerar">
+            <div class="row">
+
+                <div class="col-6">
+
+                    <label>Diretorio Base</label>
+                    <input class="form form-control" type="text" id="txtBase" name='txtBase' value="teste"><br>
                     <label>Banco de Dados :</label>
                     <select class="form form-control" id="selBanco" name="selBanco">
-                        <option>Escolha o banco</option>
-                        <?php foreach ($dados as $key => $value) {
-                          print "<option>".$value->Database."</option>";  
+                        <option>gestaocedec</option>
+                        <?php
+                        foreach ($dados as $key => $value) {
+                            print "<option>" . $value->Database . "</option>";
                         }
                         ?>
                     </select>
                     <br>
-                </div>
-                <div class="col-md-6">
+
+
                     <label>Nome Tabela (Nome da Tabela no Banco de Dados) :</label><br>
                     <select name="tabela" id="tabela"  class="form form-control" >
                         <option>Escolha a Tabela</option>                      
                     </select>
                     <br>
-                </div>
-                <div class="col-md-6">
+
                     <label>Nome Modulo ( Nome completo do Modulo ex. mod_aju :</label><br>
                     <select name="modulo" id="modulo"  class="form form-control" >
                         <option>mod_ajuda</option>
@@ -88,8 +87,7 @@
                         <option>mod_admin</option>
                     </select>
                     <br>
-                </div>
-                <div class="col-md-6">
+
                     <label>BackEnd/FrontEnd :</label><br>
                     <select name="bacFront" id="bacFront"  class="form form-control" >
                         <option>backEnd</option>
@@ -97,107 +95,151 @@
 
                     </select>
                     <br>
-                </div>
-                <div class="col-md-6">
+
                     <label>Contexto ( separação dentro de um modulo ex conEstoque :</label><br>
-                    <input type="text"  class="form form-control" name="contexto" id="contexto" value="conEstoque">
+                    <input type="text"  class="form form-control" name="contexto" id="contexto" >
                     <br>
+                    <button type="button" class="btn btn-primary" name="btnPreview" id="btnPreview">Preview</button>
+                    <button type="button" class="btn btn-primary" name="btnGerar" id="btnGerar">Gerar</button>
+
+
+
                 </div>
+                <div class="col" id="path">
 
-                <button type="button" class="btn btn-primary" name="btnPreview" id="btnPreview">Preview</button>
-
-                <br>
-                <br>
-                 <div class="col-md-12" id='pathModel'>
-                    <span>Model :
-                        <input class="form form-control" type="text" name="txtModel" id='txtModel' value="c:\wampp\www\teste">
+                    <label>Caminho Model :</label>
+                    <input class="form form-control" type="text" name="txtModel" id='txtModel' >
                     </span>
-                 
-                <br>
-                 
-                    <span>Controller :
-                        <input class="form form-control" type="text" name="txtController" id='txtController' value="c:\wampp\www\teste">
+
+                    <br>
+
+                    <label>Caminho Controller :</label>
+                    <input class="form form-control" type="text" name="txtController" id='txtController' >
                     </span>
-                 
-                <br>
-                 
-                    <span>Views :<br>
-                        <input class="form form-control" type="text" name="txtView" id='txtController' value="c:\wampp\www\teste">
+
+                    <br>
+
+                    <label>Caminho Views :</label>
+                    <input class="form form-control" type="text" name="txtView" id='txtView' >
                     </span>
-                 </div>
-                
-            </form>
-
-        </div>
-        
-        <script src="js/jquery/jquery.js"></script>
-        <script src="js/bootstrap.js"></script>
-        <script type="text/javascript">
-
-            $(document).ready(function () {
-                
-                $("#pathModel").hide();
-                
-                $("#selBanco").change(function(){
-                    
-                    var form_data = new FormData();
-                    
-                    form_data.append("database", $("#selBanco").val());
-                    form_data.append("action", "database");
-                   $.ajax({
-				type: 'POST',
-				url: 'database.php',
-				cache: false,
-				contentType: false,
-				processData: false,
-				data: form_data,
-				success: function(response) {
-                                        $("#tabela").empty();
-                                        $("#tabela").append(response);
-                                        console.log(JSON.stringify(response));
-                                        
-				},
-				error: function(e){
-					console.log(JSON.stringify(form_data));
-					console.log(JSON.stringify(response));
-					alert("Ocorreu um Erro !");
-				}
-				
-			});
-                    
-                });
-
-                $("#btnPreview").click(function () {
-                    $("#pathModel").show();
-                    
-                    $("#txtModel").val("");
-                    $("#txtController").val("");
-                    $("#txtView").val("");
-                    
-                    $("#txtModel").val($("#txtModel").val() + "\\"+ $("#modulo").val() + "\\Model"); 
-                    $("#txtController").val($("#txtController").val() + "\\"+ $("#modulo").val() + "\\Controller"); 
-                    $("#txtModel").val($("#txtModel").val() + "\\"+ $("#modulo").val() + "\\Model"); 
-                })
-
-            });
 
 
-        </script>
-    </body>
+                </div>
+        </form>
+    </div>
+</div>
+  
+<script src="js/jquery/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+        $("#path").hide();
+        $("#btnGerar").hide();
+
+        var banco = "gestaocedec";
+        buscaTabela(banco);
+
+        $("#selBanco").change(function () {
+
+            banco = $("#selBanco").val();
+            buscaTabela(banco);
+        });
+
+        $("#btnPreview").click(function () {
+
+            $("#btnPreview").hide();
+            $("#btnGerar").show();
+
+            var contexto = $("#tabela").val();
+
+            if ($("#contexto").val().length > 0) {
+                contexto = $("#contexto").val();
+            }
+
+
+            $("#path").show();
+
+            $("#txtModel").val("");
+            $("#txtController").val("");
+            $("#txtView").val("");
+
+            $("#txtModel").val($("#txtModel").val() + "" + "\\" + $("#modulo").val() + "\\" + $("#bacFront").val() + "\\Model");
+            $("#txtController").val($("#txtController").val() + "\\" + $("#modulo").val() + "\\" + $("#bacFront").val() + "\\Controller");
+            $("#txtView").val($("#txtView").val() + "\\" + $("#modulo").val() + "\\" + $("#bacFront").val() + "\\View" + "\\" + contexto);
+        })
+
+
+
+    });
+
+    function buscaTabela(banco) {
+
+        var form_data = new FormData();
+
+        form_data.append("database", banco);
+        form_data.append("action", "database");
+        $.ajax({
+            type: 'POST',
+            url: 'database.php',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: form_data,
+            success: function (response) {
+                $("#tabela").empty();
+                $("#tabela").append(response);
+                //console.log(JSON.stringify(response));
+
+            },
+            error: function (e) {
+                //console.log(JSON.stringify(form_data));
+                //console.log(JSON.stringify(response));
+                alert("Ocorreu um Erro !");
+            }
+
+        });
+    }
+
+
+    /* gerar */
+    function gerar() {
+
+        var form = new FormData();
+
+        form.append("txtBase",       $("#txtBase").val());
+        form.append("selBanco",      $("#selBanco").val());
+        form.append("tabela",        $("#tabela").val());
+        form.append("modulo",        $("#modulo").val());
+        form.append("bacFront",      $("#bacFront").val());
+        form.append("contexto",      $("#contexto").val());
+        form.append("txtModel",      $("#txtModel").val());
+        form.append("txtController", $("#txtController").val());
+        form.append("txtView",       $("#txtView").val());
+        $.ajax({
+            type: 'POST',
+            url: 'gerador.php',
+            cache: false,
+            contentType: false,
+            processData: false,
+            data: form_data,
+            success: function (response) {
+                $("#tabela").empty();
+                $("#tabela").append(response);
+                //console.log(JSON.stringify(response));
+
+            },
+            error: function (e) {
+                //console.log(JSON.stringify(form_data));
+                //console.log(JSON.stringify(response));
+                alert("Ocorreu um Erro !");
+            }
+
+        });
+    }
+
+
+</script>
+</body>
 </html>
-
-
-<?php
-?>
-
-
-<?php
-$post = isset($_POST) ? $_POST : null;
-
-$btnEnvia = isset($_POST['envia']) ? true : false;
-
-if ($btnEnvia) {
-
-    $con = Conexao::getinstance();
-}
-?>
